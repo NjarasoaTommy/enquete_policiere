dc5cbcd6f93b20b9d1fc107e05ed78051da9b975
Njarasoa Tommy <njarasoatommy@gmail.com>
2025-08-24 22:41:04 +0300

    Show if the suspect is guilty or not when getting information about accusation

--- interface/src/app/manage-accusation-modal/manage-accusation-modal.component.ts
+++ interface/src/app/manage-accusation-modal/manage-accusation-modal.component.ts
@@ -11,17 +11,21 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ManageAccusationModalComponent implements OnInit {
   constructor(private api_service: ApiService, private route: ActivatedRoute) {}
+  facts: any[] = [];
+  resultat = '';
   ngOnInit(): void {
     this.route.queryParams.subscribe((params: any) => {
       this.getAllFacts(params.nom, params.crime);
     });
   }
-  facts: any[] = [];
   getAllFacts(nom: string, crime: string) {
     this.api_service
       .getAllFactsService(nom, crime)
       .subscribe((all_facts: any) => {
         this.facts = all_facts.facts;
       });
+    this.api_service.getResultService(nom, crime).subscribe((res: any) => {
+      this.resultat = res.result;
+    });
   }
 }
