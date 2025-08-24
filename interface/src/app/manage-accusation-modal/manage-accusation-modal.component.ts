88abdf40c7ab1e1c12dc34135dff5a082047f675
Njarasoa Tommy <njarasoatommy@gmail.com>
2025-08-24 22:22:20 +0300

    Create the component that lists all fact about a suspect

--- interface/src/app/manage-accusation-modal/manage-accusation-modal.component.ts
+++ interface/src/app/manage-accusation-modal/manage-accusation-modal.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ApiService } from '../services/api.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-manage-accusation-modal',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './manage-accusation-modal.component.html',
   styleUrl: './manage-accusation-modal.component.css',
 })
-export class ManageAccusationModalComponent {
-  constructor() {}
+export class ManageAccusationModalComponent implements OnInit {
+  constructor(private api_service: ApiService, private route: ActivatedRoute) {}
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params: any) => {
+      this.getAllFacts(params.nom, params.crime);
+    });
+  }
+  facts: any[] = [];
+  getAllFacts(nom: string, crime: string) {
+    this.api_service
+      .getAllFactsService(nom, crime)
+      .subscribe((all_facts: any) => {
+        this.facts = all_facts.facts;
+      });
+  }
 }
