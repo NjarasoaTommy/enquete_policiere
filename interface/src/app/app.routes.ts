d1741a2d826fb7a19ef3d9024597f7dc5d70d20f
Njarasoa Tommy <njarasoatommy@gmail.com>
2025-08-24 17:54:03 +0300

    Create the possible routes for listing crimes and accusations

--- interface/src/app/app.routes.ts
+++ interface/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'manage-accusation',
+    loadComponent: () =>
+      import(
+        './manage-accusation-modal/manage-accusation-modal.component'
+      ).then((m) => m.ManageAccusationModalComponent),
+  },
+  {
+    path: 'crimes',
+    loadComponent: () =>
+      import('./list-crime/list-crime.component').then(
+        (m) => m.ListCrimeComponent
+      ),
+  },
+  {
+    path: 'accusations',
+    loadComponent: () =>
+      import('./list-accusation/list-accusation.component').then(
+        (m) => m.ListAccusationComponent
+      ),
+  },
+];
