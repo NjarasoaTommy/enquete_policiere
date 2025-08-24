9af3fe99b962fc464a98f95f0bf7353f99cb4a13
Njarasoa Tommy <njarasoatommy@gmail.com>
2025-08-24 15:18:23 +0300

    Remove old routes in the project

--- interface/src/app/app.routes.ts
+++ interface/src/app/app.routes.ts
@@ -1,30 +1,3 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-  {
-    path: 'tri-color-fire',
-    loadComponent: () =>
-      import('./tri-color-fire/tri-color-fire.component').then(
-        (m) => m.TriColorFireComponent
-      ),
-  },
-  {
-    path: 'problem',
-    loadComponent: () =>
-      import('./problem/problem.component').then((m) => m.ProblemComponent),
-  },
-  {
-    path: 'wolf-goat',
-    loadComponent: () =>
-      import('./wolf-goat/wolf-goat.component').then(
-        (m) => m.WolfGoatComponent
-      ),
-  },
-  {
-    path: '',
-    loadComponent: () =>
-      import('./tri-color-fire/tri-color-fire.component').then(
-        (m) => m.TriColorFireComponent
-      ),
-  },
-];
+export const routes: Routes = [];
