c8ac90143c08283234b1969febc8734eae3a893e
Njarasoa Tommy <njarasoatommy@gmail.com>
2025-08-24 16:36:10 +0300

    Add the button for managing accusation

--- interface/src/app/list-accusation/list-accusation.component.ts
+++ interface/src/app/list-accusation/list-accusation.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ApiService } from '../services/api.service';
+import { ManageAccusationModalComponent } from '../manage-accusation-modal/manage-accusation-modal.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-list-accusation',
-  imports: [CommonModule],
+  imports: [CommonModule, MatDialogModule, MatButtonModule],
   templateUrl: './list-accusation.component.html',
   styleUrl: './list-accusation.component.css',
 })
 export class ListAccusationComponent implements OnInit {
   accusations: any[] = [];
-  constructor(private api_service: ApiService) {}
+  isAccusationManaged = false;
+  constructor(private api_service: ApiService, private dialog: MatDialog) {}
   ngOnInit() {
     this.getAllAccusations();
   }
@@ -20,5 +25,17 @@ export class ListAccusationComponent implements OnInit {
       .subscribe((all_accusations: any[]) => {
         this.accusations = all_accusations;
       });
+  }
+  manageAccusation() {
+    const dialogRef = this.dialog.open(ManageAccusationModalComponent);
+
+    dialogRef.afterClosed().subscribe((result: any) => {
+      if (result) {
+        console.log('VITA :', result);
+      }
+    });
+  }
+  closeAccusationManagementModal() {
+    this.isAccusationManaged = false;
   }
 }
